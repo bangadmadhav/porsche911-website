468f04e7959aa3f9528f53e2065532438d828968
bangadmadhav <bangadmadhav@gmail.com>
2025-01-06 23:07:12 +0530

    Inital Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Hero from './components/Hero'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <>
+      <Hero />
+      <Navbar />q
+    </>
+  )
+}
+
+export default App
